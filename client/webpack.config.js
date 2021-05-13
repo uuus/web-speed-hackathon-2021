540383e800a6ada37b26116c3fb23f6fbed64bfc
Yuki Usui <yuuki6254@gmail.com>
2021-05-13 20:38:23 +0900

    sourcemap

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,7 +20,7 @@ const config = {
       '/api': 'http://localhost:3000',
     },
   },
-  devtool: 'inline-source-map',
+  devtool: process.env.NODE_ENV === 'production' ? false : 'inline-source-map',
   entry: {
     main: [
       'core-js',
